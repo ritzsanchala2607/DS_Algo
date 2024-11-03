4688ac5b6e458f5cf153d973232b4f9daad46ae3
ritzsanchala2607 <ritzsanchala2607@gmail.com>
2024-11-03 18:37:15 +0530

    Contains LeetCode Problems Solutions

---
+++ LeetCode/496RotateString/Solution.java
@@ -0,0 +1,16 @@
+package LeetCode.RotateString;
+
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        // Check if lengths are the same; if not, s can't be rotated to become goal
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        
+        // Concatenate s with itself
+        String doubled = s + s;
+        
+        // Check if goal is a substring of the doubled string
+        return doubled.contains(goal);
+    }
+}
