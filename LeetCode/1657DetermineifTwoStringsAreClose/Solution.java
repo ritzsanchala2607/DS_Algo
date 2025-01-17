9b9675f5dfd9b1e32117d8d962f774c9380750a1
ritzsanchala2607 <ritzsanchala2607@gmail.com>
2025-01-17 21:15:04 +0530

    Implemented Solution of LeetCode Problem 1657. Determine if Two Strings Are Close

---
+++ LeetCode/1657DetermineifTwoStringsAreClose/Solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean closeStrings(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        
+        if (m != n)
+            return false;
+        
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+        
+        for (int i = 0; i < m; i++) {
+            char ch1 = word1.charAt(i);
+            char ch2 = word2.charAt(i);
+            
+            freq1[ch1-'a']++;
+            freq2[ch2-'a']++;
+        }
+        
+        //check word1 char is present in word 2 or not
+        for (int i = 0; i < 26; i++) {
+            if ((freq1[i] != 0 && freq2[i] != 0) || (freq1[i] == 0 && freq2[i] == 0))
+                continue;
+            
+            return false;
+        }
+        
+        // 2nd point = match freq
+        Arrays.sort(freq1);
+        Arrays.sort(freq2);
+        
+        return Arrays.equals(freq1, freq2);
+    }
+}
