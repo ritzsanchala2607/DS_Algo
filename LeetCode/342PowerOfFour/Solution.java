6bb51eda723fd9be35e4594ff2136aca54228f34
ritzsanchala2607 <ritzsanchala2607@gmail.com>
2025-06-23 23:22:54 +0530

    Implemented a solution of leetcode problem 342 power of four.

---
+++ LeetCode/342PowerOfFour/Solution.java
@@ -0,0 +1,13 @@
+package 342PowerOfFour;
+
+class Solution {
+     public boolean isPowerOfFour(int n) {
+        if (n == 0) {
+            return false;
+        }
+        while (n % 4 == 0) {
+            n /= 4;
+        }
+        return n == 1;
+    }
+}
